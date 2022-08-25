284148825c8fb2df4510eddd85611c44504d6080
AliGaxiola <ali.gaxiola@uabc.edu.mx>
2022-08-24 22:07:34 -0700

    Java holamundo new line

--- Test/Java/Test/HolaMundo.java
+++ Test/Java/Test/HolaMundo.java
@@ -2,5 +2,6 @@ public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola mundo!!");
         System.out.println("Soy Aligg");
+        System.out.println("Esto es un test");
     }
 }

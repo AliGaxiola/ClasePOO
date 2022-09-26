013daa371a086e153c3da96e404f8a314e31c7c8
AliGaxiola <ali.gaxiola@uabc.edu.mx>
2022-09-25 20:29:04 -0700

    Taller 2,1,1

---
+++ Taller_2,1,1/Desert.java
@@ -0,0 +1,19 @@
+public class Desert extends Bioma {
+
+	private String name;
+
+	public void getName() {
+		// TODO - implement Desert.getName
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param name
+	 */
+	public void setName(string name) {
+		// TODO - implement Desert.setName
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ Taller_2,1,1/Forest.java
@@ -0,0 +1,19 @@
+public class Forest extends Bioma {
+
+	private String name;
+
+	public void getName() {
+		// TODO - implement Forest.getName
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param name
+	 */
+	public void setName(string name) {
+		// TODO - implement Forest.setName
+		throw new UnsupportedOperationException();
+	}
+
+}
